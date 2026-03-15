f2b9e15a5f08ee40f259daba00cc3ed83df927da
Arrow-K <aomar.dlm@gmail.com>
2026-03-14 20:01:05 -0600

    feat: agregar servicio API (FastAPI) y frontend (React) con CRUD de catálogo de productos

---
+++ app/services/api/dbconfig.py
@@ -0,0 +1,39 @@
+from os import environ
+from typing import Annotated, Any, Generator
+
+# from dotenv import load_dotenv
+# from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
+
+# load_dotenv("../../.env")
+# check if the database file exists
+# If not, it will be created when the first table is created
+DATABASE_ENGINE = environ.get("DATABASE_ENGINE", "duckdb")
+DATABASE_NAME = environ.get("DATABASE_NAME", "tremenda-test")
+
+if DATABASE_ENGINE == "duckdb":
+    DATABASE_URL = "duckdb:///./data/dummy.db"
+elif DATABASE_ENGINE == "postgresql":
+    POSTGRES_USER = environ.get("POSTGRES_USER", "admin")
+    POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD", "password")
+    POSTGRES_HOST = environ.get("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = environ.get("POSTGRES_PORT", 5432)
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{DATABASE_NAME}"
+else:
+    raise ValueError(f"Unsupported DATABASE_ENGINE: {DATABASE_ENGINE}")
+
+print(f"Connecting to database at {DATABASE_URL}")
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session, Any, None]:
+    with Session(engine) as session:
+        yield session
+
+
+# SessionType = Annotated[Session, Depends(get_session)]
